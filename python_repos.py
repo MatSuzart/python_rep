a4731f5646f5e4878bd391059a84ec32ba6261a5
MatSuzart <mathsuzart@outlook.com>
2024-12-25 00:13:08 -0300

    ✅exam-repo

--- python_repos.py
+++ python_repos.py
@@ -10,6 +10,17 @@ print(f"Status code: {r.status_code}")
 
 #Converte o objeto de resposat em um dicionário
 response_dict = r.json()
+print(f"Status code: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
 
+#Explora informações sobre os repositórios
+repo_dicts = response_dict['items']
+print(f"Repositories returned:{len(repo_dicts)}")
+
+#examina o primeiro repositório
+repo_dicts = repo_dicts[0]
+print(f"\nKeys:{len(repo_dicts)}")
+for key in sorted(repo_dicts.keys()):
+    print(key)
 #Processa os resultados
 print(response_dict.keys())
