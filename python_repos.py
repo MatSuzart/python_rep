23b275c4484e23d67b136091fe4f05b9f3560b5e
MatSuzart <mathsuzart@outlook.com>
2025-01-09 23:22:19 -0300

    repo_inf

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,4 @@
-import requests
+import request
 import plotly.express as px
 #Cria uma chamada de API e verifica a resposta
 url = "https://api.github.com/search/repositories"
@@ -15,12 +15,21 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 #Explora informações sobre os repositórios
 repo_dicts = response_dict['items']
+repo_links, stars,houver_texts = [],[],[]
 print(f"Repositories returned:{len(repo_dicts)}")
 print("\nSelected information about first repository:")
 
 repo_names,stars,hover_texts = [],[]
 
 for repo_dict in repo_dicts:
+    #Transforma os nomes dos repositórios em links ativos
+    repo_name=repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_links)
+
+
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
     #Cria textos flutuantes
@@ -31,8 +40,10 @@ for repo_dict in repo_dicts:
     #Cria a visualização
     title = "Most-Starred Python Projects on GitHub"
     labels = {'x':'Repository', 'y':'Stars'}
-    fig = px.bar(x=repo_names,y=stars, title= title, labels=labels,haver_name=hover_texts)
+    fig = px.bar(x=repo_links,y=stars,title=title,labels=labels,hover_name=hover_texts)
+    #fig = px.bar(x=repo_names,y=stars, title= title, labels=labels,haver_name=hover_texts)
     fig.update_layout(title_font_size=28, xaxis_title_font_size=20,yaxis_title_font_size=20)
+    fig.update_traces(maker_color='SteelBlue',maker_opacity=0.6)
     fig.show()
 
     print(f"Name:{repo_dict['name']}")
