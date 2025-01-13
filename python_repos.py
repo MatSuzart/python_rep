d16a0ec7f6809f5aa1862f72372b629028b6e9df
MatSuzart <mathsuzart@outlook.com>
2025-01-13 03:27:37 -0300

    💥repos_request

--- python_repos.py
+++ python_repos.py
@@ -1,11 +1,11 @@
-import request
+import requests
 import plotly.express as px
 #Cria uma chamada de API e verifica a resposta
 url = "https://api.github.com/search/repositories"
 url +="?language:python+sort:stars+stars:>1000"
 
 headers = {"Accept":"application/vnd.github.v3+json"}
-r = request.get(url,headers=headers)
+r = requests.get(url,headers=headers)
 print(f"Status code: {r.status_code}")
 
 #Converte o objeto de resposat em um dicionário
